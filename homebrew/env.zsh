f92166950356824f18722c34449c2e715b3b869b
Lars Wassermann <lars.wassermann@samedi.de>
2025-01-30 16:18:48 +0100

    Fix asdf-postgres libcrypto problems

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -6,3 +6,6 @@ export HOMEBREW_NO_EMOJI=1
 
 # Disable lock-file generation (these are for debugging).
 export HOMEBREW_BUNDLE_NO_LOCK=1
+
+# Set this value to fix some problems with finding openssl libcrypto e.g. in asdf-postgres
+export HOMEBREW_PREFIX=/opt/homebrew
